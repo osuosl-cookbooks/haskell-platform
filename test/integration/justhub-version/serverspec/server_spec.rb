c0839bf63be9e56518ec0614d06c06b7f09a47d5
Lance Albertson <lance@osuosl.org>
2014-07-02 14:02:22 -0700

    Check if the file exists before we try to get version information from it

--- test/integration/justhub-version/serverspec/server_spec.rb
+++ test/integration/justhub-version/serverspec/server_spec.rb
@@ -8,9 +8,9 @@ include Serverspec::Helper::DetectOS
       it { should be_installed.with_version('1-4.0.el6') }
   end
 end
-describe command('/usr/bin/ghc --version | awk \'{print $NF}\'') do
-    it { should return_stdout '7.4.1' }
-end
 describe file('/usr/bin/ghc') do
     it { should be_executable }
+end
+describe command('/usr/bin/ghc --version | awk \'{print $NF}\'') do
+    it { should return_stdout '7.4.1' }
 end
